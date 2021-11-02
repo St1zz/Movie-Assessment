1ffef94efe99c347a577153786a4ff2998a58449
Justin Wallen <wallenju@gmail.com>
2021-11-02 14:27:47 -0400

    yeah this fire

--- Movie Assessment/ViewController.swift
+++ Movie Assessment/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var scene: UIView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var movieImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    var images = [UIImage(named: "coraline"), UIImage(named: "drStrange"), UIImage(named: "scottPilgrim"), UIImage(named: "mugenTrain"), UIImage(named: "princessFrog")]
+    
+    var currentImage = 0
+    
+    @IBAction func nextTouch(_ sender: Any) {
+        currentImage += 1
+        if currentImage == images.count {
+             currentImage = 0
+        }
+        print(currentImage)
+        movieImage.image = images[currentImage]
+    }
+    
+    @IBAction func backTouch(_ sender: Any) {
+        currentImage -= 1
+        if currentImage == 0 {
+            currentImage = images.count
+        }
+        print(currentImage - 1)
+        movieImage.image = images[currentImage -  1]
+    }
+    
+    
 }
 
